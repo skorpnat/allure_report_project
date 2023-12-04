ea51a225b27c99bf73f0f269422a9bcb35c9485f
Klimova Natalia <skorpnat@gmail.com>
2023-12-04 18:25:56 +0600

    generate Allure Report

---
+++ src/test/java/allure/SelenideTest.java
@@ -0,0 +1,25 @@
+package allure;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+    @Test
+    public void testIssueSearch() {
+        open("https://github.com");
+        $(".search-input").click();
+        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+        $("#query-builder-test").pressEnter();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#80")).should(Condition.exist);
+
+    }
+
+}
